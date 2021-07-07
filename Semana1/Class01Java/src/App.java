72fccbaae37cf706a28f750d371b8930e343c6d8
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-07-06 21:33:46 -0500

    Do While

--- Semana1/Class01Java/src/App.java
+++ Semana1/Class01Java/src/App.java
@@ -4,25 +4,24 @@ public class App {
     public static void main(String[] args) throws Exception {
         uso_if();
         float_promedio();
-        char letra;
-        Scanner leer = new Scanner(System.in);
-        System.out.println("Digite la letra: ");
-        letra = leer.next().charAt(0);
-        letra = Character.toUpperCase(letra);
-        switch(letra){
-            case 'a': case 'A':System.out.println("Avion");
-            break;
-            case 'e': System.out.println("Elefante");
-            break;
-            case 'i': System.out.println("Iglesia");
-            break;
-            case 'o': System.out.println("Oso");
-            break;
-            case 'u': System.out.println("Uvas");
-            break;
-            default: System.out.println("No es Vocal");   
-        }
+        uso_char();
 
+        int opcion;
+        float area = 0;
+        opcion = seleccionar_menu();
+        switch(opcion) {
+            case 1: area = calcular_circulo();
+            System.out.println("Area del Circulo: " + area);
+            break;
+            case 2: area = calcular_triangulo();
+            System.out.println("Area del Triangulo: " + area);
+            break;
+            case 3: area = calcular_rectangulo();
+            System.out.println("Area del Rectangulo: " + area);
+            break;
+            case 4: System.exit(0);
+            break
+        }
 
     }
     // Aqui van todos los procedicimientos o funciones
@@ -56,6 +55,47 @@ public class App {
         
         float promedio = (nota1 + nota2 + nota3)/ 3;
         System.out.println("El estudiante "+nombre+" tiene el promedio"+promedio+"");
+    }
+
+    public static void uso_char(){
+        char letra;
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Digite la letra: ");
+        letra = leer.next().charAt(0);
+        letra = Character.toUpperCase(letra);
+        switch(letra){
+            case 'a': case 'A':System.out.println("Avion");
+            break;
+            case 'e': System.out.println("Elefante");
+            break;
+            case 'i': System.out.println("Iglesia");
+            break;
+            case 'o': System.out.println("Oso");
+            break;
+            case 'u': System.out.println("Uvas");
+            break;
+            default: System.out.println("No es Vocal");   
+        }
+    }
+
+    public static int seleccionar_menu(){
+        int opc;
+        Scanner leer = new Scanner(System.in);
+        do{
+            System.out.println("MENU PRINCIPAL ");
+            System.out.println("1. Calcular area Circulo ");
+            System.out.println("2. Calcular area Triangulo");
+            System.out.println("3. Calcular area Rectangulo");
+            System.out.println("4. Salir");
+            System.out.println("Seleccione su opcion -> ");
+
+        } while(opcion != 4);
+    }
+    public static float calcular_circulo(){
+        float radio;
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Digite el radio: ");
+        radio
     }
 
 }
