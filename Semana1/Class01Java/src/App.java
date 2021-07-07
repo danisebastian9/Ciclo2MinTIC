2396ef53f69bc4136f6ee8240f631059ec498fa7
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-07-06 19:59:03 -0500

    First If exer

--- Semana1/Class01Java/src/App.java
+++ Semana1/Class01Java/src/App.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        uso_if();
+
     }
+    // Aqui van todos los procedicimientos o funciones
+    public static void uso_if(){
+        System.out.println("Digite el numero");
+        Scanner leer = new Scanner(System.in);
+        int numero;
+        numero = leer.nextInt();
+        if (numero == 0) 
+            System.out.println("El numero es CERO");
+         else if(numero % 2 == 0)
+            System.out.println("El numero es PAR");
+         else
+            System.out.println("El numero es IMPAR");
+        // Cuando es mas de una instruccion es necesaria las llaves
+        
+
+    }
+
 }
