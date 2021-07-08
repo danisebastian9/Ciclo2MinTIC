ca3904b7875300cf242ae3b389c85c046edcd00f
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-07-07 23:40:52 -0500

    Do While fixed

--- Semana1/Class01Java/src/App.java
+++ Semana1/Class01Java/src/App.java
@@ -8,20 +8,22 @@ public class App {
 
         int opcion;
         float area = 0;
-        opcion = seleccionar_menu();
-        switch(opcion) {
-            case 1: area = calcular_circulo();
-            System.out.println("Area del Circulo: " + area);
-            break;
-            case 2: area = calcular_triangulo();
-            System.out.println("Area del Triangulo: " + area);
-            break;
-            case 3: area = calcular_rectangulo();
-            System.out.println("Area del Rectangulo: " + area);
-            break;
-            case 4: System.exit(0);
-            break
-        }
+        do {
+            opcion = seleccionar_menu();
+            switch(opcion) {
+                case 1: area = calcular_circulo();
+                System.out.println("Area del Circulo: " + area);
+                break;
+                case 2: area = calcular_triangulo();
+                System.out.println("Area del Triangulo: " + area);
+                break;
+                case 3: area = calcular_rectangulo();
+                System.out.println("Area del Rectangulo: " + area);
+                break;
+                case 4: System.exit(0);
+                break;
+            } 
+        } while (opcion != 4);
 
     }
     // Aqui van todos los procedicimientos o funciones
@@ -85,15 +87,15 @@ public class App {
     public static int seleccionar_menu(){
         int opc;
         Scanner leer = new Scanner(System.in);
-        do{
-            System.out.println("MENU PRINCIPAL ");
-            System.out.println("1. Calcular area Circulo ");
-            System.out.println("2. Calcular area Triangulo");
-            System.out.println("3. Calcular area Rectangulo");
-            System.out.println("4. Salir");
-            System.out.println("Seleccione su opcion -> ");
+        System.out.println("MENU PRINCIPAL ");
+        System.out.println("1. Calcular area Circulo ");
+        System.out.println("2. Calcular area Triangulo");
+        System.out.println("3. Calcular area Rectangulo");
+        System.out.println("4. Salir");
+        System.out.println("Seleccione su opcion -> ");
+        opc = leer.nextInt();
+        return opc;
 
-        } while(opcion != 4);
     }
     public static float calcular_circulo(){
         float radio;
