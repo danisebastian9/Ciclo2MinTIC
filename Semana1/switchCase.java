31d1df14314101bf155841075bbd4ae97804299e
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-07-12 18:50:45 -0500

    Project01Week2

--- Semana1/switchCase.java
+++ Semana1/switchCase.java
@@ -2,51 +2,53 @@ import java.util.Scanner;
 
 public class switchCase {
     public static void main(String[] arg){
-        byte opcion = 0;
+        byte opcion = 1;
         int num1 = 0, num2 = 0, res = 0;
         
         Scanner ingreso = new Scanner(System.in);
-
-        System.out.println("Digite la Opcion: \n1. Suma\n2. Resta\n3. Multiplicacion\n4. Division\n5. Modulo");
-        opcion = Byte.parseByte(ingreso.nextLine());
-
-        switch(opcion){
-            case 1: System.out.println("Suma");
-            System.out.println("Digite los numeros a Sumar: ");
-            num1 = Integer.parseInt(ingreso.nextLine());
-            num2 = Integer.parseInt(ingreso.nextLine());
-            res = num1 + num2;
-            System.out.println(num1 + " + " + num2 + " = " + res);
-            break;
-            case 2: System.out.println("Resta");
-            System.out.println("Digite los numeros a Restar: ");
-            num1 = Integer.parseInt(ingreso.nextLine());
-            num2 = Integer.parseInt(ingreso.nextLine());
-            res = num1 - num2;
-            System.out.println(num1 + " - " + num2 + " = " + res);
-            break;
-            case 3: System.out.println("Multiplicacion");
-            System.out.println("Digite los numeros a Multiplicar: ");
-            num1 = Integer.parseInt(ingreso.nextLine());
-            num2 = Integer.parseInt(ingreso.nextLine());
-            res = num1 * num2;
-            System.out.println(num1 + " * " + num2 + " = " + res);
-            break;
-            case 4: System.out.println("Division");
-            System.out.println("Digite los numeros a Dividir: ");
-            num1 = Integer.parseInt(ingreso.nextLine());
-            num2 = Integer.parseInt(ingreso.nextLine());
-            res = num1 / num2;
-            System.out.println(num1 + " / " + num2 + " = " + res);
-            break;
-            case 5: System.out.println("Modulo");
-            System.out.println("Digite los numeros a sacar modulo: ");
-            num1 = Integer.parseInt(ingreso.nextLine());
-            num2 = Integer.parseInt(ingreso.nextLine());
-            res = num1 % num2;
-            System.out.println(num1 + " mod " + num2 + " = " + res);
-            break;
-            default: System.out.println("No pertenece a ninguna opcion");
+        while(opcion >= 1 && opcion <= 5){
+            System.out.println("Digite la Opcion: \n1. Suma\n2. Resta\n3. Multiplicacion\n4. Division\n5. Modulo");
+            opcion = Byte.parseByte(ingreso.nextLine());
+    
+            switch(opcion){
+                case 1: System.out.println("Suma");
+                System.out.println("Digite los numeros a Sumar: ");
+                num1 = Integer.parseInt(ingreso.nextLine());
+                num2 = Integer.parseInt(ingreso.nextLine());
+                res = num1 + num2;
+                System.out.println(num1 + " + " + num2 + " = " + res);
+                break;
+                case 2: System.out.println("Resta");
+                System.out.println("Digite los numeros a Restar: ");
+                num1 = Integer.parseInt(ingreso.nextLine());
+                num2 = Integer.parseInt(ingreso.nextLine());
+                res = num1 - num2;
+                System.out.println(num1 + " - " + num2 + " = " + res);
+                break;
+                case 3: System.out.println("Multiplicacion");
+                System.out.println("Digite los numeros a Multiplicar: ");
+                num1 = Integer.parseInt(ingreso.nextLine());
+                num2 = Integer.parseInt(ingreso.nextLine());
+                res = num1 * num2;
+                System.out.println(num1 + " * " + num2 + " = " + res);
+                break;
+                case 4: System.out.println("Division");
+                System.out.println("Digite los numeros a Dividir: ");
+                num1 = Integer.parseInt(ingreso.nextLine());
+                num2 = Integer.parseInt(ingreso.nextLine());
+                res = num1 / num2;
+                System.out.println(num1 + " / " + num2 + " = " + res);
+                break;
+                case 5: System.out.println("Modulo");
+                System.out.println("Digite los numeros a sacar modulo: ");
+                num1 = Integer.parseInt(ingreso.nextLine());
+                num2 = Integer.parseInt(ingreso.nextLine());
+                res = num1 % num2;
+                System.out.println(num1 + " mod " + num2 + " = " + res);
+                break;
+                default: System.out.println("No pertenece a ninguna opcion");
+            }
         }
+        ingreso.close();
     }
 }
---
+++ Semana2/Exer01/Exer01/src/exer01/Exer01.java
@@ -0,0 +1,17 @@
+package exer01;
+
+/**
+ *
+ * @author sebas
+ */
+public class Exer01 {
+    public static void main(String[] args) {
+        
+        int x = 1;
+        while(x <= 5){
+            System.out.println("Hola Mundo");
+            x++;
+        }
+    }
+    
+}
