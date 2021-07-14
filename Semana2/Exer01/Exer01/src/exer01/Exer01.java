76dc1571f64f417c1b44c5a233a9ae7afae7bdbd
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-07-13 19:22:15 -0500

    For added

--- Semana2/Exer01/Exer01/src/exer01/Exer01.java
+++ Semana2/Exer01/Exer01/src/exer01/Exer01.java
@@ -1,5 +1,7 @@
 package exer01;
 
+import java.util.Scanner;
+
 /**
  *
  * @author sebas
@@ -11,6 +13,22 @@ public class Exer01 {
         while(x <= 5){
             System.out.println("Hola Mundo");
             x++;
+        }
+        
+        x = 0;
+        
+        do{
+            System.out.println("Hola Muchachos....");
+            x++;
+        }while(x <= 2);
+        
+        int num;
+        Scanner ingreso = new Scanner(System.in);
+        num = ingreso.nextInt();
+        if (num > 1){
+            for(int i=1; i <= num ; i++ ){
+                System.out.println("Manejando for..");
+            }
         }
     }
     
