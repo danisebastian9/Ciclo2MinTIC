020769b2ef6e03167b202884a40390841b538eda
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-07-13 23:44:22 -0500

    Methods added

--- Semana2/POOexer01/src/pooexer01/Automovil.java
+++ Semana2/POOexer01/src/pooexer01/Automovil.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pooexer01;
 
-/**
- *
- * @author sebas
- */
+
 public class Automovil {
+    public String marca = "", color, placa, estado;
+    public int modelo, ruedas = 0, No_puertas;
     
+    public void arrancar(){ // void = metodo no retorna un valor
+        if(this.estado.equals("Detenido")){ // this = ubicado sobre la misma clase de este atributo. .equals comparar con string en vez de == 
+            System.out.println("Arrancando...");
+            this.estado = "En Movimiento";
+        } else{
+            System.out.println("El auto ya esta en movimiento...");
+
+        }
+    }
+    
+    public int numeroRuedas(){ // int del tipo de dato que va a retornar
+        
+        return this.ruedas;
+    }
+    
+    public String modificaColor(String color){
+        this.color = color;
+        return this.color;
+    }
 }
