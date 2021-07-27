e8fc0328cb4ad4547c2e5f2cc6450ab218d79321
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-07-27 15:02:49 -0500

    Child estudiante

--- Semana3/class0716/src/estudiante.java
+++ Semana3/class0716/src/estudiante.java
@@ -1,3 +1,16 @@
-public class estudiante {
+public class estudiante extends persona { // Hereda de persona
+    
+    private int codigoEst;
+    private float notaFinal; 
+
+    // Constructor se tiene que crear
+
+    public estudiante(String nombre, String apellidos, int edad, int codigoEst, float notaFinal){
+        super(nombre, apellidos, edad);
+        this.codigoEst = codigoEst;
+        this.notaFinal = notaFinal;
+
+    }
+
     
 }
