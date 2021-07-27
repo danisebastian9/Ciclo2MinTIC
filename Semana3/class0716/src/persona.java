c2bbd1ae0c6dbd9540ee50929d3820867112bb93
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-07-27 14:55:41 -0500

    Inheritance Persona

--- Semana3/class0716/src/persona.java
+++ Semana3/class0716/src/persona.java
@@ -1,3 +1,33 @@
 public class persona {
     
+    private String Nombre;
+    private String Apellidos;
+    private int edad;
+    
+    
+    public persona(String nombre, String apellidos, int edad) { // Constructor
+        Nombre = nombre;
+        Apellidos = apellidos;
+        this.edad = edad;
+    }
+
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+
+    public int getEdad() {
+        return edad;
+    }
+
+    
+
+
+    
+    
 }
