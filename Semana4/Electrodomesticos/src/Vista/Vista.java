c7337b356bd598d2e04e1a46f95052d4e3f0b540
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-07-29 20:22:22 -0500

    Vista added

--- Semana4/Electrodomesticos/src/Vista/Vista.java
+++ Semana4/Electrodomesticos/src/Vista/Vista.java
@@ -1,5 +1,60 @@
 package Vista;
 
+import java.util.Scanner;
+
 public class Vista {
+
+    Scanner ingreso = new Scanner(System.in);
     
+    public byte mostrarMenu(){
+        byte opcion;
+        System.out.println("1. Crear Lavadora \n2. Crear televisor \n3. Salir");
+        System.out.println("Ingrese Opcion: ");
+        opcion = ingreso.nextByte();
+        return opcion;
+    }
+
+    public double capturaPrecioBase(){
+        System.out.println("Ingrese Precio Base: ");
+        double precio = ingreso.nextDouble();
+        return precio;
+    }
+    
+    public String capturaColor(){
+        System.out.println("Ingrese Color: ");
+        String dato = ingreso.next();
+        return dato;
+    }
+
+    public char capturaConsumo(){
+        System.out.println("Ingrese Consumo Energetico: ");
+        char dato = ingreso.next() .charAt(0);
+        return dato;
+    }
+
+    public float capturaPeso(){
+        System.out.println("Ingrese Peso: ");
+        float dato = ingreso.nextFloat();
+        return dato;
+    }
+
+    public int capturaCarga(){
+        System.out.println("Ingrese Carga de Lavadora: ");
+        int dato = ingreso.nextInt();
+        return dato;
+    }
+
+    public int capturaResolucion(){
+        System.out.println("Ingrese Resolucion del TV: ");
+        int dato = ingreso.nextInt();
+        return dato;
+    }
+
+    public boolean capturaTdt(){
+        System.out.println("Ingrese Sintonizador TDT del TV: ");
+        boolean dato = ingreso.nextBoolean();
+        return dato;
+    }
+
+
 }
