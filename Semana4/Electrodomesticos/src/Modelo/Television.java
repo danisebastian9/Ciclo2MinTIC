c0b250098849cae0b4d5a3eaae970679b986a22a
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-07-29 20:08:47 -0500

    Television class added

--- Semana4/Electrodomesticos/src/Modelo/Television.java
+++ Semana4/Electrodomesticos/src/Modelo/Television.java
@@ -1,5 +1,46 @@
 package Modelo;
 
-public class Television {
+public class Television extends Electrodomestico{
     
+    private int resolucion;
+    private boolean tdt;
+
+    public Television(){
+        resolucion = 20;
+        tdt = false;
+    }
+
+    public Television(double precioBase, float peso) {
+        super(precioBase, peso);
+        resolucion = 20;
+        tdt = false;
+    }
+
+    public Television(double precioBase, String color, char consumoEnergetico, float peso, int resolucion,
+            boolean tdt) {
+        super(precioBase, color, consumoEnergetico, peso);
+        this.resolucion = resolucion;
+        this.tdt = tdt;
+    }
+
+    public int getResolucion() {
+        return resolucion;
+    }
+
+
+    public boolean isTdt() {
+        return tdt;
+    }
+
+    public void precioFinal(){
+        super.precioFinal();
+        if(resolucion > 40){
+            precioBase += precioBase * 0.3;
+        }
+
+        if(tdt){
+            precioBase += 50;
+        }
+    }
+
 }
