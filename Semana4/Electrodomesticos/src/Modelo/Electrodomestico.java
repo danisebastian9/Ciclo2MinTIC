4eb844394b7f87c272ab5e847192dfa714677574
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-07-29 19:56:30 -0500

    lavadora class created

--- Semana4/Electrodomesticos/src/Modelo/Electrodomestico.java
+++ Semana4/Electrodomesticos/src/Modelo/Electrodomestico.java
@@ -10,17 +10,19 @@ public class Electrodomestico {
 
     public Electrodomestico(){
         precioBase = 100;
-        color = "Blanco";
+        color = "blanco";
         consumoEnergetico = 'F'; // Char comilla sencilla, char una sola letra o digito
         peso = 5;
+        comprobarConsumoEnergetico(consumoEnergetico);
     }
 
 
     public Electrodomestico(double precioBase, float peso) {
         this.precioBase = precioBase;
         this.peso = peso;
-        color = "Blanco";
+        color = "blanco";
         consumoEnergetico = 'F'; // Char comilla sencilla, char una sola letra o digito
+        comprobarConsumoEnergetico(consumoEnergetico);
     }
 
 
@@ -29,9 +31,121 @@ public class Electrodomestico {
         this.color = color;
         this.consumoEnergetico = consumoEnergetico;
         this.peso = peso;
+        comprobarConsumoEnergetico(consumoEnergetico);
     }
 
+
+    public double getPrecioBase() {
+        return precioBase;
+    }
+
+
+    public void setPrecioBase(double precioBase) {
+        this.precioBase = precioBase;
+    }
+
+
+    public String getColor() {
+        return color;
+    }
+
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
+    public char getConsumoEnergetico() {
+        return consumoEnergetico;
+    }
+
+
+    public void setConsumoEnergetico(char consumoEnergetico) {
+        this.consumoEnergetico = consumoEnergetico;
+    }
+
+
+    public float getPeso() {
+        return peso;
+    }
+
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    private void comprobarConsumoEnergetico(char letra){
+        switch(letra){
+            case 'A':
+            break;
+            case 'B':
+            break;
+            case 'C':
+            break;
+            case 'D':
+            break;
+            case 'E':
+            break;
+            case 'F':
+            break;
+            default:
+            consumoEnergetico = 'F';
+            break;
+        }
+    }
+
+
+    private void comprobarColor(String color){
+        switch(color.toLowerCase()){
+            case "blanco":
+            break;
+            case "negro":
+            break;
+            case "rojo":
+            break;
+            case "azul":
+            break;
+            case "gris":
+            break;
+            default:
+            this.color = "blanco";
+            break;
+        }
+    }
     
-    
+    public void precioFinal(){
+        switch(consumoEnergetico){
+            case 'A':
+            precioBase += 100;
+            break;
+            case 'B':
+            precioBase += 80;
+            break;
+            case 'C':
+            precioBase += 60;
+            break;
+            case 'D':
+            precioBase += 50;
+            break;
+            case 'E':
+            precioBase += 30;
+            break;
+            case 'F':
+            precioBase += 10;
+            break;
+            default:
+            break;
+        }
+
+        if (peso >= 0 && peso <= 19){
+           precioBase += 10; 
+        } else if (peso >= 20 && peso <= 49){
+            precioBase += 50;
+        } else if (peso >= 50 && peso <= 79){
+            precioBase += 80;
+        } else if (peso >= 80){
+            precioBase += 100;
+        }
+    }
 
 }
--- Semana4/Electrodomesticos/src/Modelo/Lavadora.java
+++ Semana4/Electrodomesticos/src/Modelo/Lavadora.java
@@ -1,5 +1,34 @@
 package Modelo;
 
-public class Lavadora {
+public class Lavadora extends Electrodomestico{
     
+
+    private int carga;
+
+    public Lavadora(){
+        carga = 5;
+    }
+
+    public Lavadora(double precioBase, float peso) {
+        super(precioBase, peso);
+        carga = 5;
+    }
+
+    public Lavadora(double precioBase, String color, char consumoEnergetico, float peso, int carga) {
+        super(precioBase, color, consumoEnergetico, peso);
+        this.carga = carga;
+    }
+
+    public int getCarga() {
+        return carga;
+    }
+
+    public void precioFinal(){
+        super.precioFinal(); // Llamando al metodo de la clase padre
+        if(carga > 30){
+            precioBase += 50;
+        }
+    }
+    
+
 }
