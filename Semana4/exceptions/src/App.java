cee3d15d3b1733a8fc824eb769dfcd18ca6a549a
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-07-30 22:26:56 -0500

    Exception exer

--- Semana4/exceptions/src/App.java
+++ Semana4/exceptions/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int num1 = 10;
+        int num2 = 0;
+        int division;
+
+        try{
+            division = num1 / num2;
+        }catch(ArithmeticException ex){
+            division = 1;
+            System.out.println("Error...." + ex.getMessage());
+        }
+        System.out.println("Respuesta: " + division);
+
     }
 }
