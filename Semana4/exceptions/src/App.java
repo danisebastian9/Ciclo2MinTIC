c4216cfd2b915e49ad4e5753ecc5b40ed05020ff
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-07-30 23:09:01 -0500

    exceptions

--- Semana4/exceptions/src/App.java
+++ Semana4/exceptions/src/App.java
@@ -1,3 +1,6 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         int num1 = 10;
@@ -11,6 +14,29 @@ public class App {
             System.out.println("Error...." + ex.getMessage());
         }
         System.out.println("Respuesta: " + division);
+
+
+        Scanner ingreso = new Scanner(System.in);
+        int numero1= 0;
+        try{
+            numero1 = ingreso.nextInt();
+        }catch(InputMismatchException excep){
+            System.out.println("El dato de entrada debe ser un numero");
+            System.out.println("Error en ingreso de datos "+ excep.getMessage());
+        }
+        System.out.println("El numero que dijito es: " + numero1);
+
+
+        int[] datos = new int[30];
+        try{
+            datos[-5] = 45;
+        }catch(Exception exptn){
+            System.out.println("El indice esta fuera del tamaño del arreglo: " + exptn.getMessage());
+        }
+        System.out.println("Continua el programa... ");
+
+        
+
 
     }
 }
