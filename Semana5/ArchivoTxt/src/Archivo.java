f65de3f97d317d8a362fa6e7ea740393d43271da
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-08-03 00:10:16 -0500

    File read

--- Semana5/ArchivoTxt/src/Archivo.java
+++ Semana5/ArchivoTxt/src/Archivo.java
@@ -1,3 +1,24 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Archivo {
     
+    public void leer(){
+        
+        try {
+            FileReader arch = new FileReader("C:\\Users\\sebas\\Documents\\Development\\MinTic\\Ciclo2MinTIC\\Semana5\\ArchivoTxt\\src\\archivo31.txt");
+            int datos = arch.read();
+            while(datos != -1){
+                datos = arch.read();
+                System.out.println(datos);
+            }
+        } catch (IOException e) {
+            System.out.println("No se encontro el archivo");
+            e.printStackTrace();
+        }
+
+
+
+    }
 }
