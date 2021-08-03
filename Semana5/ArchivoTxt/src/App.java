fc897b96a32c06ed31698cd2a809fd3758c480ef
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-08-03 00:33:03 -0500

    Writing added

--- Semana5/ArchivoTxt/src/App.java
+++ Semana5/ArchivoTxt/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         Archivo prueba = new Archivo();
         prueba.leer();
+        prueba.escribir();
     }
 }
--- Semana5/ArchivoTxt/src/Archivo.java
+++ Semana5/ArchivoTxt/src/Archivo.java
@@ -1,5 +1,6 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Archivo {
@@ -8,17 +9,38 @@ public class Archivo {
         
         try {
             FileReader arch = new FileReader("C:\\Users\\sebas\\Documents\\Development\\MinTic\\Ciclo2MinTIC\\Semana5\\ArchivoTxt\\src\\archivo31.txt");
-            int datos = arch.read();
+            int datos = 0;
             while(datos != -1){
-                datos = arch.read();
-                System.out.println(datos);
+                datos = arch.read(); // trae el numero del caracter
+                char c = (char)datos; // Convertir en caracter
+                System.out.print(c);
             }
+
+            arch.close(); // Cerrar archivo
+
         } catch (IOException e) {
-            System.out.println("No se encontro el archivo");
+            System.out.println("No se encontro el archivo, no se pudo leer...");
             e.printStackTrace();
         }
+    }
 
 
+    public void escribir(){
+
+        String texto = "Probando la escritura en archivos con Java";
+        try {
+            FileWriter escritura = new FileWriter("src\\escribir31.txt");
+            for(int i = 0; i < texto.length(); i++){
+                escritura.write(texto.charAt(i));
+            }
+            
+            escritura.close();
+            
+
+        } catch (IOException e) {
+            System.out.println("No se encontro el archivo, no se pudo leer...");
+            e.printStackTrace();
+        }
 
     }
 }
