ec377a4d4e20702c90ac394a214ab70a4e120144
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-08-04 23:15:24 -0500

    Array byte

--- Semana5/ArchivoTxt/src/Imagen.java
+++ Semana5/ArchivoTxt/src/Imagen.java
@@ -4,16 +4,26 @@ import java.io.IOException;
 public class Imagen {
     
     public void leerImagen(){ // Stream byte
+
+        int[] datos = new int[54083];
+
+        int contador = 0;
+
         try{
             FileInputStream leerImg = new FileInputStream("src\\PcImage.jpg");
             int byteEntrada = 0;
             while(byteEntrada != -1){
                 byteEntrada = leerImg.read();
-                System.out.println(byteEntrada);
+                if(byteEntrada != -1){
+                    datos[contador] = byteEntrada; // Guardar cada byte en el arreglo
+                    System.out.println(datos[contador]); // Imprime lo que esta en el arreglo.
+                }
+                contador++;
             }
             leerImg.close();
         }catch(IOException ex){
-
+            System.out.println("No se encontro el archivo..." + ex);
         }
+        // System.out.print(contador); // Cantidad de Bytes de la imagen / archivo. 
     }
 }
