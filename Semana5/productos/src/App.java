74b267fc7eeb3610ea7b8e0d6bc63340cd55fc4d
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-08-05 00:13:25 -0500

    Get byte data from file

--- Semana5/productos/src/App.java
+++ Semana5/productos/src/App.java
@@ -1,5 +1,7 @@
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
@@ -17,14 +19,25 @@ public class App {
         Lista.add(new Productos(2027,"Escoba",8000));
 
         try {
-            ObjectOutputStream escribirProducto = new ObjectOutputStream( new FileOutputStream("src\\listaProductos.txt"));
+            ObjectOutputStream escribirProducto = new ObjectOutputStream( new FileOutputStream("src\\listaProductos.dat"));
             escribirProducto.writeObject(Lista);
             escribirProducto.close();
 
-        } catch (IOException e) {
+            
+            // Leer informacion de archivo .dat
+            ObjectInputStream recuperar = new ObjectInputStream(new FileInputStream("src\\listaProductos.dat"));
+            ArrayList<Productos> Leer = new ArrayList<Productos>();
+            Leer = (ArrayList<Productos>)recuperar.readObject();
+            recuperar.close();
+            for(Productos p:Leer){
+                System.out.println(p.mostrarProducto());
+            }
+
+        } catch (Exception e) {
             //TODO: handle exception
         }
 
+        System.out.println("--------------------------------------");
         for(Productos p:Lista){
             System.out.println(p.mostrarProducto());
         }
