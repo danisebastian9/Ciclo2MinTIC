25df19d4d4ee7bd1d3e02f807d58c01afd2fdd4b
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-08-10 23:07:36 -0500

    Access the DB

--- Semana6/SQL1/src/App.java
+++ Semana6/SQL1/src/App.java
@@ -2,5 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         // MYSQL added with XAMPP
+        // Enable MySQL and APACHE, 
+        // Then through the browser go to localhost
+        // Then to phpmyadmin in order to access the DB
     }
 }
