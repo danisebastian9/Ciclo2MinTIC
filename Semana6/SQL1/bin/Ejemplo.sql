07cc22465446f44e79678ea2f10288e0d062a4b7
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-08-11 23:18:01 -0500

    Comments finished

--- Semana6/SQL1/bin/Ejemplo.sql
+++ Semana6/SQL1/bin/Ejemplo.sql
@@ -1,1 +1,6 @@
--- SQL - es el Lenguaje de consulta estructural
+-- SQL - es el Lenguaje de consulta estructural - Crear y Administrar BD
+-- DDL - Lenguaje de definicion de datos - Crear estructura BD
+
+-- DML - Lenguaje de manipulacion de datos - CRUD 
+
+-- DCL - Lenguaje de control de datos - Seguridad de los datos
