b8171741a54c3e3329159b70886a714fee6a858b
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-08-11 23:25:55 -0500

    First SQL added

--- Semana6/SQL1/src/Ejemplo.sql
+++ Semana6/SQL1/src/Ejemplo.sql
@@ -1,6 +1,13 @@
 -- SQL - es el Lenguaje de consulta estructural - Crear y Administrar BD
 -- DDL - Lenguaje de definicion de datos - Crear estructura BD
 
+CREATE database Libros_31;
+
+use libros_31; -- Seleccionar la BD 
+
+
+
+
 -- DML - Lenguaje de manipulacion de datos - CRUD 
 
 -- DCL - Lenguaje de control de datos - Seguridad de los datos
