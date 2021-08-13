4e6cfe2794771bcd78bfc6431cb76c4ef95a5286
Daniel Sebastian Cubides <44168842+danisebastian9@users.noreply.github.com>
2021-08-12 21:19:58 -0500

    Update App.java

--- Semana6/SQL1/src/App.java
+++ Semana6/SQL1/src/App.java
@@ -7,5 +7,6 @@ public class App {
         // Then to phpmyadmin in order to access the DB
         // Disco local C > XAMPP
         // testSQL DB created
+        // Today's lesson is going to be seen tomorrow. 
     }
 }
