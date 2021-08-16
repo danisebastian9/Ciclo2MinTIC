264958b13f07ce164a0cd7199e7a46316c197dd8
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-08-15 23:46:51 -0500

    MVC added

---
+++ Semana6/Lista_Productos/src/Controlador/Controlador.java
@@ -0,0 +1,53 @@
+package Controlador;
+
+//import Modelo.Archivo;
+import Modelo.Inventario;
+import Modelo.Productos;
+import Vista.Vista;
+
+public class Controlador {
+    
+    Vista vis = new Vista();
+    Inventario lista = new Inventario();
+    //Archivo  arch = new Archivo();
+
+    public void Control(){
+
+        byte opcion = 0;
+        opcion = vis.mostrarMenu();
+        String mens = "";
+
+        while(opcion >= 1 && opcion <= 6){
+            switch(opcion){
+                case 1 :
+                    Productos p = new Productos(vis.capturaRef(), vis.capturaNombre(), vis.capturaPrecio());
+                    lista.agregarProducto(p);
+                    break;
+                case 2 :
+                    mens = lista.buscarProducto(vis.capturaRef()).mostrarProducto();
+                    vis.imprimir(mens);
+                    break;
+                case 3 :
+                    lista.modificarProducto(vis.capturaRef(), vis.capturaNombre(), vis.capturaPrecio());
+                    break;
+                case 4 : 
+                    lista.eliminarProducto(vis.capturaRef());
+                break;
+                case 5 :
+                //lista.setLista(arch.leerArchivo(arch.getArchivo()));;
+                    for(Productos prods: lista.getLista()){
+                        vis.imprimir(prods.mostrarProducto());
+                    }
+                    break;
+                case 6 : 
+                    vis.imprimir("Hasta Pronto");
+                    System.exit(0);
+            
+            }
+            opcion = vis.mostrarMenu();
+        }
+
+
+    }
+    
+}
---
+++ Semana6/Lista_Productos/src/Modelo/Inventario.java
@@ -0,0 +1,58 @@
+package Modelo;
+
+import java.util.ArrayList;
+
+public class Inventario {
+    
+    //CRUD : Create, Read, Update, Delete
+    public ArrayList<Productos> lista = new ArrayList<Productos>();
+
+    //Archivo  arch = new Archivo();
+
+
+    public void agregarProducto(Productos p){ // Metodo agregar a la lista
+        lista.add(p);
+        //arch.escribirArchivo(lista, arch.getArchivo());
+    }
+
+    public Productos buscarProducto(String ref){ // Metodo buscar por referencia
+        //lista = arch.leerArchivo(arch.getArchivo());
+        Productos z = null; 
+        for(Productos p:lista){
+            if(p.getReferencia().equals(ref)){
+                z = p; 
+                return p;
+            }
+        }
+        return z; 
+    }
+
+    public void modificarProducto(String ref, String nom, long pre){  // Metodo para Modificar
+        //lista = arch.leerArchivo(arch.getArchivo());
+        int contador = 0;
+        for(Productos p:lista){
+            if(p.getReferencia().equals(ref)){
+                lista.set(contador, new Productos(ref, nom, pre));
+            }
+            contador++;
+        }
+        //arch.escribirArchivo(lista, arch.getArchivo());
+    }
+
+    public void eliminarProducto(String ref){  // Metodo para Eliminar
+        //lista = arch.leerArchivo(arch.getArchivo());
+        Productos p = this.buscarProducto(ref);
+        lista.remove(p);
+        //arch.escribirArchivo(lista, arch.getArchivo());
+    }
+
+    public ArrayList<Productos> getLista() {
+        return lista;
+    }
+
+    public void setLista(ArrayList<Productos> lista) {
+        this.lista = lista;
+    }
+    
+
+}
