0599b818b4bf8c1fd89ce8cee0f6736592205f2d
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-08-19 14:41:39 -0500

    GUI JOptionPane

--- Semana7/GUI/src/App.java
+++ Semana7/GUI/src/App.java
@@ -7,5 +7,17 @@ public class App {
         JOptionPane.showMessageDialog(null, "Mensaje Advertencia", "Este es el titulo", 2);
         JOptionPane.showMessageDialog(null, "Mensaje Error", "Este es el titulo", 0);
 
+        String nombre = JOptionPane.showInputDialog(null, "Digite su nombre: ");
+        JOptionPane.showMessageDialog(null, "Bienvenido " + nombre);
+
+        int numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite su numero: "));
+        JOptionPane.showMessageDialog(null, "Bienvenido " + numero);
+
+        int op = JOptionPane.showConfirmDialog(null, "Desea iniciar sesion?");
+        JOptionPane.showMessageDialog(null, op);
+
+        String opcion[] = {"Sumar","Restar","Dividir","Multiplicar"};
+        int ope = JOptionPane.showOptionDialog(null, "Seleccione Operacion: ", "Ventana operaciones", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null,opcion, opcion[0]);
+        JOptionPane.showMessageDialog(null, ope);
     }
 }
